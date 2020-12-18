8874bf4e00d550eaded2d25c4ea758f4104a00a9
ragdoll <museopkim0214@gmail.com>
2020-12-19 05:31:41 +0900

    feat: Rearrange dependencies by DI container

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,16 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void join() {
-
-        MemberService memberService = new MemberServiceImpl();
-
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
